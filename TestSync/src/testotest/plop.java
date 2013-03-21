9156b5106c29d9836b45f5dcf7a04791e76a5115
Thomas <thms.sch@gmail.com>
2013-03-21 11:06:49 +0100

    Ajout du projet plop

---
+++ TestSync/src/testotest/plop.java
@@ -0,0 +1,13 @@
+package testotest;
+
+public class plop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Salut");
+	}
+
+}
