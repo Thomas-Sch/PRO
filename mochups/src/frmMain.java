215092a81bd8b0e3a060ed35948198b0745a7998
Thomas <thms.sch@gmail.com>
2013-04-05 18:48:52 +0200

    Premier jet de l'interface

--- mochups/src/frmMain.java
+++ mochups/src/frmMain.java
@@ -1,7 +1,8 @@
-
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.*;
-
+import javax.swing.border.BevelBorder;
 
 /**
 * This code was edited or generated using CloudGarden's Jigloo
@@ -36,6 +37,31 @@ public class frmMain extends javax.swing.JFrame {
 	private JMenuItem mniEditAccountConsult;
 	private JMenuItem mniEditAccountAdd;
 	private JMenuItem mniShowListInterval;
+	private JComboBox cmbSubCategory;
+	private JLabel lblTransaction;
+	private JButton jButton1;
+	private JLabel jLabel4;
+	private JTextField jTextField3;
+	private JLabel jLabel1;
+	private JTextField jTextField1;
+	private JComboBox cmbTransactionMode;
+	private JComboBox cmbAccount;
+	private JComboBox cmbBudget;
+	private JSeparator jSeparator1;
+	private JSeparator sepNorthMiddle;
+	private JButton btnBudgetAccept;
+	private JTextField txtReason;
+	private JLabel lblReason;
+	private JComboBox cmbCategory;
+	private JTextField txtDate;
+	private JLabel lblDate;
+	private JTextField txtAmount;
+	private JLabel lblAmount;
+	private JLabel lblAddExpense;
+	private JLabel lblLastFive;
+	private JLabel lblWarning;
+	private JPanel pnlLastFive;
+	private JPanel pnlWarnings;
 	private JMenuItem mniShowListLast20;
 	private JMenu mnuShowList;
 	private JMenu mnuShowGraphic;
@@ -53,7 +79,7 @@ public class frmMain extends javax.swing.JFrame {
 	private JMenu mnuEdit;
 	private JMenuItem mniFileQuit;
 	private JMenu mnuFile;
-	private JMenuBar jMenuBar1;
+	private JMenuBar mnbMain;
 
 	/**
 	* Auto-generated main method to display this JFrame
@@ -75,13 +101,173 @@ public class frmMain extends javax.swing.JFrame {
 	
 	private void initGUI() {
 		try {
-			setSize(400, 300);
+			getContentPane().setLayout(null);
 			{
-				jMenuBar1 = new JMenuBar();
-				setJMenuBar(jMenuBar1);
+				pnlWarnings = new JPanel();
+				getContentPane().add(pnlWarnings);
+				pnlWarnings.setBounds(10, 48, 266, 125);
+				pnlWarnings.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+			}
+			{
+				pnlLastFive = new JPanel();
+				getContentPane().add(pnlLastFive);
+				pnlLastFive.setBounds(319, 47, 241, 125);
+				pnlLastFive.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+			}
+			{
+				lblWarning = new JLabel();
+				getContentPane().add(lblWarning);
+				lblWarning.setText("Alertes");
+				lblWarning.setBounds(10, 27, 34, 14);
+			}
+			{
+				lblLastFive = new JLabel();
+				getContentPane().add(lblLastFive);
+				lblLastFive.setText("Dernières transactions");
+				lblLastFive.setBounds(319, 27, 108, 14);
+			}
+			{
+				lblAddExpense = new JLabel();
+				getContentPane().add(lblAddExpense);
+				lblAddExpense.setText("Ajouter une dépense");
+				lblAddExpense.setBounds(10, 202, 101, 14);
+			}
+			{
+				ComboBoxModel cmbBudgetModel = 
+						new DefaultComboBoxModel(
+								new String[] { "Budget", "Maison", "Tourisme", "Cadeaux", "Nouveau budget..." });
+				cmbBudget = new JComboBox();
+				getContentPane().add(getCmbBudget());
+				cmbBudget.setModel(cmbBudgetModel);
+				cmbBudget.setBounds(10, 227, 90, 20);
+			}
+			{
+				lblAmount = new JLabel();
+				getContentPane().add(lblAmount);
+				lblAmount.setText("Montant:");
+				lblAmount.setBounds(119, 230, 44, 14);
+			}
+			{
+				txtAmount = new JTextField();
+				getContentPane().add(txtAmount);
+				txtAmount.setBounds(173, 227, 78, 20);
+			}
+			{
+				lblDate = new JLabel();
+				getContentPane().add(lblDate);
+				lblDate.setText("Date:");
+				lblDate.setBounds(263, 230, 27, 14);
+			}
+			{
+				txtDate = new JTextField();
+				getContentPane().add(txtDate);
+				txtDate.setBounds(300, 227, 63, 20);
+			}
+			{
+				ComboBoxModel cmbCategoryModel = 
+						new DefaultComboBoxModel(
+								new String[] { "Item One", "Item Two" });
+				cmbCategory = new JComboBox();
+				getContentPane().add(cmbCategory);
+				cmbCategory.setModel(cmbCategoryModel);
+				cmbCategory.setBounds(377, 227, 92, 20);
+			}
+			{
+				ComboBoxModel cmbSubCategoryModel = 
+						new DefaultComboBoxModel(
+								new String[] { "Item One", "Item Two" });
+				cmbSubCategory = new JComboBox();
+				getContentPane().add(cmbSubCategory);
+				cmbSubCategory.setModel(cmbSubCategoryModel);
+				cmbSubCategory.setBounds(479, 227, 81, 20);
+			}
+			{
+				lblReason = new JLabel();
+				getContentPane().add(lblReason);
+				lblReason.setText("Motif:");
+				lblReason.setBounds(10, 256, 28, 14);
+			}
+			{
+				txtReason = new JTextField();
+				getContentPane().add(txtReason);
+				txtReason.setText("Saisissez le motif de la dépense...");
+				txtReason.setBounds(48, 258, 315, 66);
+			}
+			{
+				btnBudgetAccept = new JButton();
+				getContentPane().add(btnBudgetAccept);
+				btnBudgetAccept.setText("Valider");
+				btnBudgetAccept.setBounds(443, 280, 65, 23);
+			}
+			{
+				sepNorthMiddle = new JSeparator();
+				getContentPane().add(sepNorthMiddle);
+				sepNorthMiddle.setBounds(10, 186, 550, 10);
+			}
+			{
+				jSeparator1 = new JSeparator();
+				getContentPane().add(jSeparator1);
+				jSeparator1.setBounds(10, 333, 550, 11);
+			}
+			{
+				lblTransaction = new JLabel();
+				getContentPane().add(lblTransaction);
+				lblTransaction.setText("Ajouter une transaction");
+				lblTransaction.setBounds(10, 345, 121, 14);
+			}
+			{
+				ComboBoxModel cmbAccountModel = 
+						new DefaultComboBoxModel(
+								new String[] { "Compte","Poste Courant", "Epargne", "Ajouter un nouveau compte..." });
+				cmbAccount = new JComboBox();
+				getContentPane().add(cmbAccount);
+				cmbAccount.setModel(cmbAccountModel);
+				cmbAccount.setBounds(10, 365, 90, 20);
+			}
+			{
+				ComboBoxModel cmbTransactionModeModel = 
+						new DefaultComboBoxModel(
+								new String[] { "Débit", "Crédit" });
+				cmbTransactionMode = new JComboBox();
+				getContentPane().add(cmbTransactionMode);
+				cmbTransactionMode.setModel(cmbTransactionModeModel);
+				cmbTransactionMode.setBounds(119, 365, 77, 20);
+			}
+			{
+				jTextField1 = new JTextField();
+				getContentPane().add(jTextField1);
+				jTextField1.setBounds(268, 365, 78, 20);
+			}
+			{
+				jLabel1 = new JLabel();
+				getContentPane().add(jLabel1);
+				jLabel1.setText("Montant:");
+				jLabel1.setBounds(214, 368, 44, 14);
+			}
+			{
+				jTextField3 = new JTextField();
+				getContentPane().add(jTextField3);
+				jTextField3.setText("Saisissez le motif de la dépense...");
+				jTextField3.setBounds(48, 406, 315, 66);
+			}
+			{
+				jLabel4 = new JLabel();
+				getContentPane().add(jLabel4);
+				jLabel4.setText("Motif:");
+				jLabel4.setBounds(10, 406, 28, 14);
+			}
+			{
+				jButton1 = new JButton();
+				getContentPane().add(jButton1);
+				jButton1.setText("Valider");
+				jButton1.setBounds(443, 406, 65, 23);
+			}
+			{
+				mnbMain = new JMenuBar();
+				setJMenuBar(mnbMain);
 				{
 					mnuFile = new JMenu();
-					jMenuBar1.add(mnuFile);
+					mnbMain.add(mnuFile);
 					mnuFile.setText("Fichier");
 					{
 						mniFileQuit = new JMenuItem();
@@ -91,7 +277,7 @@ public class frmMain extends javax.swing.JFrame {
 				}
 				{
 					mnuEdit = new JMenu();
-					jMenuBar1.add(mnuEdit);
+					mnbMain.add(mnuEdit);
 					mnuEdit.setText("Edition");
 					{
 						mnuEditAccount = new JMenu();
@@ -169,7 +355,7 @@ public class frmMain extends javax.swing.JFrame {
 				}
 				{
 					mnuShow = new JMenu();
-					jMenuBar1.add(mnuShow);
+					mnbMain.add(mnuShow);
 					mnuShow.setText("Affichage");
 					{
 						mnuShowGraphic = new JMenu();
@@ -194,7 +380,7 @@ public class frmMain extends javax.swing.JFrame {
 				}
 				{
 					mnuInterrogation = new JMenu();
-					jMenuBar1.add(mnuInterrogation);
+					mnbMain.add(mnuInterrogation);
 					mnuInterrogation.setText("?");
 					{
 						mniInterrogationHelp = new JMenuItem();
@@ -208,9 +394,15 @@ public class frmMain extends javax.swing.JFrame {
 					}
 				}
 			}
+			pack();
+			this.setSize(586, 561);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public JComboBox getCmbBudget() {
+		return cmbBudget;
 	}
 
 }
