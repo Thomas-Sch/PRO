20c5bfd067668899bb02a1e7e8ca7857e514c2e8
Thomas <thms.sch@gmail.com>
2013-04-12 15:19:11 +0200

    Initial commit

---
+++ interface/src/InterfaceLauncher.java
@@ -0,0 +1,33 @@
+/* ============================================================================
+ * Nom du fichier   : InterfaceLauncher.java
+ * ============================================================================
+ * Date de création : 12 avr. 2013
+ * ============================================================================
+ * Auteurs          : Biolzi Sébastien
+ *                    Brito Carvalho Bruno
+ *                    Decorvet Grégoire
+ *                    Schweizer Thomas
+ *                    Sinniger Marcel
+ * ============================================================================
+ */
+
+/**
+ * Launcher du programme pour tester les interfaces.
+ * @author Biolzi Sébastien
+ * @author Brito Carvalho Bruno
+ * @author Decorvet Grégoire
+ * @author Schweizer Thomas
+ * @author Sinniger Marcel
+ *
+ */
+public class InterfaceLauncher {
+
+   /**
+    * @param args Options de lancement.
+    */
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
