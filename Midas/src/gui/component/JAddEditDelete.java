06159c3ac7fb55a40f2638ba4745024fbfdae978
Thomas <thms.sch@gmail.com>
2013-05-09 11:26:40 +0200

    + Méthodes permettant de lier des actions listener.

--- Midas/src/gui/component/JAddEditDelete.java
+++ Midas/src/gui/component/JAddEditDelete.java
@@ -13,12 +13,13 @@
 package gui.component;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
- * Contient les bouttons pour modifier une liste de catégorie ou auteurs.
+ * Contient les bouttons pour modifier une liste standard.
  * @author Biolzi Sébastien
  * @author Brito Carvalho Bruno
  * @author Decorvet Grégoire
@@ -33,19 +34,53 @@ public class JAddEditDelete extends JPanel {
     */
    private static final long serialVersionUID = -2871705858912856162L;
    
-   private JButton btnNew = new JButton("Ajouter");
-   private JButton btnModify = new JButton("Modifier");
-   private JButton btnDelete = new JButton("Supprimer");
+   private JButton btnAdd;
+   private JButton btnModify;
+   private JButton btnDelete;
 
    /**
     * Constructeur par défaut.
     */
    public JAddEditDelete() {
       setLayout(new FlowLayout(FlowLayout.LEFT));
+      buildContent();
+   }
+   
+   /**
+    * Construction des éléments grahpiques du panel.
+    */
+   private void buildContent() {
+      btnAdd = new JButton("Ajouter");
+      add(btnAdd);
       
-      add(btnNew);
+      btnModify = new JButton("Modifier");
       add(btnModify);
+      
+      btnDelete = new JButton("Supprimer");
       add(btnDelete);
    }
+   
+   /**
+    * Ajout d'une action sur le bouton btnAdd.
+    * @param listener Action à effectuer.
+    */
+   public void addAddActionListener(ActionListener listener) {
+      btnAdd.addActionListener(listener);
+   }
+   
+   /**
+    * Ajout d'une action sur le bouton btnModify.
+    * @param listener Action à effectuer.
+    */
+   public void addModifyActionListener(ActionListener listener) {
+      btnModify.addActionListener(listener);
+   }
 
+   /**
+    * Ajout d'une action sur le bouton btnDelete.
+    * @param listener Action à effectuer.
+    */
+   public void addDeleteActionListener(ActionListener listener) {
+      btnDelete.addActionListener(listener);
+   }
 }
