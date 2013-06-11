b3e886bc93de254b04a58820b40d3e8f24f9d472
Thomas <thms.sch@gmail.com>
2013-06-11 20:29:41 +0200

    Merge II

--- Midas/src/gui/controller/MainFrame.java
+++ Midas/src/gui/controller/MainFrame.java
@@ -26,37 +26,41 @@ import core.Core;
 
 /**
  * Contrôleur du contenu de la fenêtre principale.
+ * 
  * @author Biolzi Sébastien
  * @author Brito Carvalho Bruno
  * @author Decorvet Grégoire
  * @author Schweizer Thomas
  * @author Sinniger Marcel
- *
+ * 
  */
 public class MainFrame extends Controller {
-   
+
    JMainFrame mainFrame;
-   
+
    /**
+    * Crée le contrôleur du contenu de la fenêtre principale.
     * 
+    * @param core - le coeur logique du programme.
     */
    public MainFrame(Core core) {
       super(core);
    }
-   
+
    @Override
    protected void initComponents() {
       mainFrame = new JMainFrame(this, Text.APP_TITLE.toString());
       mainFrame.setSize(Settings.mainFrame.width, Settings.mainFrame.height);
-      Positions.setPositionOnScreen(mainFrame,  Settings.mainFrame.anchor);
+      Positions.setPositionOnScreen(mainFrame, Settings.mainFrame.anchor);
       mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       mainFrame.setVisible(true);
    }
-   
+
    @Override
    protected void initListeners() {
       // Nous n'écoutons rien sur cette fenêtre.
    }
+
    @Override
    public Component getGraphicalComponent() {
       return null;
